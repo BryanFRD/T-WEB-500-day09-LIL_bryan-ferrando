a86c7971888f9081f4c3de1a32b98b3a4618204d
Bryan <bryan.ferrando@epitech.eu>
2023-10-05 10:43:43 +0100

    task04

--- task04.php
+++ task04.php
@@ -3,14 +3,14 @@
 function whoami(){
   $name = $_POST["name"];
   $age = $_POST["age"];
-  
-  if(isset($name) && isset($age)){
+  $validAge = isset($age) && is_numeric($age);
+  if(isset($name) && $validAge){
     echo "Hi, I have no name and I'm $age years old.\n";
   } else if(isset($name)){
     echo "Hi, my name is $name.\n";
-  } else if(isset($age)){
+  } else if($validAge){
     echo "Hi, I have no name and I'm $age years old.\n";
   } else {
-    echo "Hi, I have no name.\n";
+    echo "Hi\n";
   }
 }
